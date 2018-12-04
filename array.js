c1ddc3d321a4faf4a8a95a2da55d0da90f4c87e3
Prashant Mani <pmani157@gmail.com>
2018-12-04 23:41:47 +0530

    array exercise added

---
+++ array.js
@@ -0,0 +1,7 @@
+const superHeroes=['batman','iron man','spiderman','Thor']
+console.log(superHeroes)
+console.log(superHeroes[0])
+console.log(superHeroes[2])
+console.log(superHeroes[superHeroes.length-1])
+console.log(`we have ${superHeroes.length} super heroes`)
+
