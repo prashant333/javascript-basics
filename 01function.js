fd142e1bf85a5949f6b29ee63c9daa20d5604e8c
prashant333 <pmani157@gmail.com>
2018-12-05 11:39:40 +0530

    default parameters

---
+++ 01function.js
@@ -0,0 +1,10 @@
+let myMultiplier = function(num1, num2){ 
+    return num1*num2
+}
+console.log(myMultiplier(2,3))
+
+let guestUser =function(name = 'Raj',courseCount= 0){ //default parameters are teh fallback values.
+    console.log('hello '+name +' and your course count is: '+courseCount)
+}
+guestUser() // when no parameters are passed default values takes over.
+guestUser('Ram',3)
