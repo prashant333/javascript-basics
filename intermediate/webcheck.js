1400af2e374a0c78897f1fc4dc3f94dede120adb
Prashant Mani <pmani157@gmail.com>
2018-12-08 18:35:09 +0530

    username and password checker

---
+++ intermediate/webcheck.js
@@ -0,0 +1,19 @@
+let useName = 'ravi123'
+let password = '123@121'
+
+let userCheck = function(myString){
+    if ((myString.includes(123)) && (myString.length > 5)) {
+        return true
+    }
+     return false
+}
+
+let passwordCheck = function(myString){
+    if ((myString.includes('@')) && (myString.length > 6)) {
+        return true
+    }
+    return false
+}
+
+console.log(userCheck(useName))
+console.log(passwordCheck(password))
