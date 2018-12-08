3beb770f4c50818324d67140540d7fdab752ebc8
prashant333 <pmani157@gmail.com>
2018-12-08 10:35:58 +0530

    trello version 2 example updated

--- intermediate/trellov2.js
+++ intermediate/trellov2.js
@@ -1,5 +1,18 @@
 let myTodo ={
     day:'Monday',
     meeting:0,
-    meetingDone:0
+    meetingDone:0,
+    addMeeting: function(num){
+        console.log(`Number of meeting today ${myTodo.meeting= myTodo.meeting+ num}`)
+    },
+    meetDone: function(num){
+        console.log(`Number of meeting left today is ${this.meetingDone= this.meeting-num}`)
+    },
+    summary: function(){
+        console.log(`Number of meeting done is: ${this.meeting - this.meetingDone}`)
+    }
 }
+
+myTodo.addMeeting(4)
+myTodo.meetDone(1)
+myTodo.summary()
