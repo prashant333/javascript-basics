ea5fc464de2ce7605553a4842d222424a4cab9a7
Prashant Mani <pmani157@gmail.com>
2018-12-09 13:15:47 +0530

    random number generation

---
+++ intermediate/dice.js
@@ -0,0 +1,16 @@
+// let numOne=22
+// let numTwo=7
+
+// result = numOne/numTwo
+// console.log(result.toFixed(3))
+// let floatRes= result.toFixed(3)
+
+// console.log(Math.floor(floatRes))
+// console.log(Math.ceil(floatRes))
+
+// console.log(Math.random()*10) // Math.random() gives random value between zero and 1 and we multiply to get a number of our desire.
+
+let upper = 25
+let lower = 20
+let myRandom = Math.floor(Math.random()*(upper-lower+1)) + lower
+console.log(myRandom)
