6c90726c2f14a941ac1ea0c9000f04085ea12074
Prashant Mani <pmani157@gmail.com>
2018-12-10 17:07:24 +0530

    js automatic intrepretation of true and false

---
+++ advance/coercion.js
@@ -0,0 +1,23 @@
+// console.log('5'-5)
+// const giveType = typeof {}
+// console.log(giveType)
+
+// const adder =true +5 // true is treated as 1 and false as 0
+// console.log(adder)
+
+const loginDetails=[]
+// logic for getting detail for DB
+
+const loginID=loginDetails[0]
+if (loginDetails) { 
+    console.log('Authentication successful')
+    
+}else
+console.log('authentication failed')
+
+// values that are interpretted as false in js
+// false
+// 0
+// "" an empty string
+// null
+// undefined, apart from these averything else is considered as true
