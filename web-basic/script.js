d8bef3411a0fe05293d3e8fc87bcef0cd4c45dc5
prashant333 <pmani157@gmail.com>
2018-12-19 09:24:49 +0530

    script updated

--- web-basic/script.js
+++ web-basic/script.js
@@ -6,5 +6,21 @@
 // const myPElement = document.querySelectorAll('p')
 // myPElement.forEach(p=>p.textContent='changed using javaScript')
 
-document.querySelectorAll('p').forEach(p => p.textContent = "Changed using javaScript") 
+// document.querySelectorAll('p').forEach(p => p.textContent = "Changed using javaScript") 
     
+
+// var sales = 'Toyota';
+
+// function carTypes(name) {
+//   if (name === 'Honda') {
+//     return name;
+//   } else {
+//     return "Sorry, we don't sell " + name + ".";
+//   }
+// }
+
+// var car = { myCar: 'Saturn', getCar: carTypes('Honda'), special: sales };
+
+// console.log(car.myCar);   // Saturn
+// console.log(car.getCar);  // Honda
+// console.log(car.special); // Toyota
