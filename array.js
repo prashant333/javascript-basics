76b90451c28a8fedbc1e8940ba5cf42e33148943
Prashant Mani <pmani157@gmail.com>
2019-01-28 21:38:12 +0530

    array passed as an argument

--- array.js
+++ array.js
@@ -1,7 +1,20 @@
-const superHeroes=['batman','iron man','spiderman','Thor']
-console.log(superHeroes)
-console.log(superHeroes[0])
-console.log(superHeroes[2])
-console.log(superHeroes[superHeroes.length-1])
-console.log(`we have ${superHeroes.length} super heroes`)
+// const superHeroes=['batman','iron man','spiderman','Thor']
+// console.log(superHeroes)
+// console.log(superHeroes[0])
+// console.log(superHeroes[2])
+// console.log(superHeroes[superHeroes.length-1])
+// console.log(`we have ${superHeroes.length} super heroes`)
+
+// passing arguments in array
+
+console.log(max(1,-3,10,20))
+function max(...numbers) {
+    let result = -Infinity
+    for(let number of numbers){
+        if (number > result) {
+            result = number
+        }
+    }
+    return result
+}
 
