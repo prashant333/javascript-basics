749348a0073331a42dd220998f31ae184b2bbfef
Prashant Mani <pmani157@gmail.com>
2019-01-29 11:20:02 +0530

    nested array multiplication

--- array.js
+++ array.js
@@ -7,14 +7,27 @@
 
 // passing arguments in array
 
-console.log(max(1,-3,10,20))
-function max(...numbers) {
-    let result = -Infinity
-    for(let number of numbers){
-        if (number > result) {
-            result = number
+// console.log(max(1,-3,10,20))
+// function max(...numbers) {
+//     let result = -Infinity
+//     for(let number of numbers){
+//         if (number > result) {
+//             result = number
+//         }
+//     }
+//     return result
+// }
+
+// nested array multiplication
+function multiply(arr) {
+    var product = 1;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            product *= arr[i][j]
+            
         }
     }
-    return result
-}
+    return product
+} 
+console.log(multiply([[1,2],[332,3],[3,4]]))
 
