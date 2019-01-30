300b0856e8969d1e78e26ed643e7d8539cb3d894
Prashant Mani <pmani157@gmail.com>
2019-01-30 23:15:23 +0530

    switch statement

---
+++ intermediate/switch.js
@@ -0,0 +1,28 @@
+let sv= getLetter("abgk")
+console.log(sv)
+function getLetter(text) {
+    switch (text[0]) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            return 'A'
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'f':
+        case 'g':
+            return 'B';
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+            return 'C';
+        default:
+            return 'D';
+                
+            
+    }    
+}
