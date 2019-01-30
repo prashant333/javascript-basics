178286831407196fcf0e57920f9e10b3c2b93ce4
Prashant Mani <pmani157@gmail.com>
2019-01-31 00:18:28 +0530

    modifying array element

--- intermediate/arrayTraversing.js
+++ intermediate/arrayTraversing.js
@@ -1,17 +1,17 @@
 let nums= [1,2,3]
-modifyArray(nums)
-function modifyArray(nums) {
-    for (let i = 0; i < nums.length; i++){
-        if (nums[i] % 2 == 0) {
-            nums[i]= nums[i]*2
+// modifyArray(nums)
+// function modifyArray(nums) {
+//     for (let i = 0; i < nums.length; i++){
+//         if (nums[i] % 2 == 0) {
+//             nums[i]= nums[i]*2
             
-        }
-        else nums[i] = nums[i]*3
+//         }
+//         else nums[i] = nums[i]*3
         
-    }
-    console.log(nums)
+//     }
+//     console.log(nums)
     
-}
+// }
 
 // arrow function to traverse an array
 // let z= [1,3,4,5]
@@ -27,3 +27,15 @@ function modifyArray(nums) {
 //                 }
 //     console.log(arr)
 // }
+
+console.log(modifyArray(nums))
+function modifyArray(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        // Set default multiply
+        const mult = (nums[i] % 2 === 1) ? 3 : 2;
+        
+        nums[i] = nums[i] * mult;
+    }
+    
+    return nums;
+}
