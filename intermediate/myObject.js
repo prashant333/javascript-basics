7bd63361924c6c6c26e20d9b4dedc3e95de985ad
prashant333 <pmani157@gmail.com>
2019-02-01 10:57:09 +0530

    Object example added

--- intermediate/myObject.js
+++ intermediate/myObject.js
@@ -1,6 +1,15 @@
-let myYouTubeVideo={
-    title:'javaScript course',
-    price:1000,
-    author:'Prashant Mani'
-}
-console.log(`New YouTube video on javaScript named ${myYouTubeVideo.title} by ${myYouTubeVideo.author}`)
+// let myYouTubeVideo={
+//     title:'javaScript course',
+//     price:1000,
+//     author:'Prashant Mani'
+// }
+// console.log(`New YouTube video on javaScript named ${myYouTubeVideo.title} by ${myYouTubeVideo.author}`)
+
+// will work in browser.
+let fruit = prompt("Which fruit to buy?", "apple");
+
+let bag = {
+  [fruit]: 5, // the name of the property is taken from the variable fruit
+};
+
+alert( bag.apple ); // 5 if fruit="apple"
