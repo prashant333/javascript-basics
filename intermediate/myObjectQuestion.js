7cd1440dbf1a91a7e735377ce076c9a8fefde6ff
Prashant Mani <pmani157@gmail.com>
2019-02-02 10:50:19 +0530

    Multiplying the numeric entries method- 2

--- intermediate/myObjectQuestion.js
+++ intermediate/myObjectQuestion.js
@@ -1,16 +1,36 @@
-// Multipling rhe numeric entries.
+// // Multipling rhe numeric entries. Method - 1
+// let salary = {
+//     ramesh : 400,
+//     sukesh : 400,
+//     malesh : 200,
+//     admin : "suresh"
+// } 
+// function sumSalary(obj) {
+//     let sum = 0;
+//     for (let key in obj) {
+//         if(!isNaN(obj[key])){
+//             sum += 2* (obj[key]);
+//         }
+//     }return sum
+// }
+// console.log(sumSalary(salary))
+
+// Multipling rhe numeric entries. Method - 2
 let salary = {
     ramesh : 400,
     sukesh : 400,
     malesh : 200,
     admin : "suresh"
-} 
-function sumSalary(obj) {
+}
+function sum(obj) {
     let sum = 0;
-    for (let key in obj) {
-        if(!isNaN(obj[key])){
-            sum += 2* (obj[key]);
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)&& typeof obj[key]=='number') {
+            sum += obj[key]  
+        }
+        else{
+            return 'No Numeric entries'
         }
     }return sum
 }
-console.log(sumSalary(salary))
+console.log(sum(salary))
