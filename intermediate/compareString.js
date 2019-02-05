b921b1ef9602a802ac746b6de66a3dcb8aa37f26
Prashant Mani <pmani157@gmail.com>
2019-02-05 14:53:57 +0530

    finding substring in given string

---
+++ intermediate/compareString.js
@@ -0,0 +1,16 @@
+let s ='statement'
+let t ='nte'
+let j =0
+for (let i = 0; i < s.length; i++) {
+    for (j; j < t.length; j++) {
+        if (t[j]==s[i]) {
+            j=j+1
+            break
+        }
+        break
+    }
+}
+if (j==t.length) {
+    console.log('match')
+}
+else console.log('not found')
