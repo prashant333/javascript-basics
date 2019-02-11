d3b16e21fa092341ebec40641e6ff9584b1838e6
Prashant Mani <pmani157@gmail.com>
2019-02-11 14:47:46 +0530

    calculator using a constructor

--- intermediate/calculator.js
+++ intermediate/calculator.js
@@ -1,15 +1,34 @@
-let calculator = {
-    read(){
+// let calculator = {
+//     read(){
+//         this.a = +prompt('a',0);
+//         this.b = +prompt('b',0);
+//     },
+//     sum(){
+//         return this.a + this.b;
+//     },
+//     mul(){
+//         return this.a * this.b;
+//     }
+// }
+// calculator.read();
+// alert(calculator.sum())
+// alert(calculator.mul())
+
+function Calulator() {
+    this.read= function(){
         this.a = +prompt('a',0);
         this.b = +prompt('b',0);
-    },
-    sum(){
+    };
+    this.sum = function() {
         return this.a + this.b;
-    },
-    mul(){
+    };
+    this.mul = function() {
         return this.a * this.b;
-    }
+    };
 }
-calculator.read();
-alert(calculator.sum())
-alert(calculator.mul())
+
+let calc = new Calulator()
+calc.read();
+alert(calc.sum());
+alert(calc.mul());
+
