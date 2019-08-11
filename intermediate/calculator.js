1a64dfd8ed6049b238d5e8dd4f669f87afe23a61
prashant mani <prashant.mani@qbrics.com>
2019-08-12 00:11:54 +0530

    uncommented codes

--- intermediate/calculator.js
+++ intermediate/calculator.js
@@ -1,18 +1,18 @@
-// let calculator = {
-//     read(){
-//         this.a = +prompt('a',0);
-//         this.b = +prompt('b',0);
-//     },
-//     sum(){
-//         return this.a + this.b;
-//     },
-//     mul(){
-//         return this.a * this.b;
-//     }
-// }
-// calculator.read();
-// alert(calculator.sum())
-// alert(calculator.mul())
+let calculator = {
+    read(){
+        this.a = +prompt('a',0);
+        this.b = +prompt('b',0);
+    },
+    sum(){
+        return this.a + this.b;
+    },
+    mul(){
+        return this.a * this.b;
+    }
+}
+calculator.read();
+alert(calculator.sum())
+alert(calculator.mul())
 
 function Calulator() {
     this.read= function(){
