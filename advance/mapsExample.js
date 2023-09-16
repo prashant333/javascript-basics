93a246df7daffff03560d9cd94810febf65e35e1
prashant <pmani157@gmail.com>
2023-09-16 19:46:55 +0530

    map example code

---
+++ advance/mapsExample.js
@@ -0,0 +1,37 @@
+let people = [
+    {
+        name:"Prashant",
+        age: 27,
+        position: "Project delivery",
+        company: "kazam"
+    },
+    {
+        name:"Nikshit",
+        age: 27,
+        position: "Project delivery",
+        company:"Kazam"
+    },
+    {
+        name: "Monish",
+        age: 26,
+        position: "Project Delivery",
+        company: "Kazam"
+    },
+    {
+        name: "Apurva",
+        age: 24,
+        position: "Project Delivery",
+        company: "Kazam"
+    }
+]
+
+let new_MapPeople = people.map((person)=>{
+    return {
+        firstName:person.name.toUpperCase(),
+        position:person.position,
+        company:person.company
+    }
+})
+
+console.log("Type of returned data is:",typeof(new_MapPeople))
+console.log(new_MapPeople)
