74d7505222ec48a0eb368b15d3dcd52a4833ef08
prashant <pmani157@gmail.com>
2023-10-11 07:56:17 +0530

    challenge 2 complted

--- interview_coding_questions/tempConverter.js
+++ interview_coding_questions/tempConverter.js
@@ -1,4 +1,6 @@
-// Create a function called getCelsius() that takes a temperature in 
+//Challenge 1
+
+//Create a function called getCelsius() that takes a temperature in 
 // Fahrenheit as an argument and returns the temperature in celsius.
 
 // For bonus points, write it as a one line arrow function
@@ -18,3 +20,17 @@ console.log(getCelsius(32))
 const getCelsius = (num) => ((num - 32) * 5 / 9)
 
 console.log(`The temperature is ${getCelsius(32)} \xB0C`)
+
+
+// Challenge 2
+
+// Create an arrow function called minMax() that takes in an array of numbers 
+//and returns an object with the minimum and maximum numbers in the array.
+
+
+minMax = (array) =>({
+    min: Math.min(...array),
+    max: Math.max(...array)
+})
+
+console.log(minMax([1, 2, 3, 4, 5]));
